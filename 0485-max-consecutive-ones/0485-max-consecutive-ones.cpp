88cfbf6f6a92e02eb5780897d1ad61c43b9e3799
Sandeep kumar mishra <sandeep28092006@gmail.com>
2026-08-05 11:55:49 +0530

    Time: 0 ms (100%), Space: 50.1 MB (68.73%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -4,7 +4,6 @@ public:
         int n = nums.size();
         int count=0;
         int cmax=0;
-        int j=0;
         for(int i=0;i<n;i++){
             if(nums[i]==1){
                 count=count+1;
