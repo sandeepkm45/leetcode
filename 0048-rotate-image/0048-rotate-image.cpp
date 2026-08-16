984f15a8252b07fc64c979b5414bc3a2dcf2cc74
Sandeep kumar mishra <sandeep28092006@gmail.com>
2026-08-16 09:36:32 +0530

    Time: 0 ms (100%), Space: 10.5 MB (9.44%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix[0].size();
+        vector <vector<int>> max(n, vector<int>(n));
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                max[j][n-1-i]=matrix[i][j];
+            }
+        }
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                matrix[i][j]=max[i][j];
+            }
+        }
+    }
+};
