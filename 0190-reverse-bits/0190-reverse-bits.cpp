22ae24ef4faec1e90633eeb26a1e13bfb75276ec
Sandeep kumar mishra <sandeep28092006@gmail.com>
2026-08-22 17:15:38 +0530

    Time: 6 ms (5.1%), Space: 8.8 MB (5.38%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        vector <int> v;
+        int no = n;
+        long long int sum = 0;
+        long long int x = 1;
+        while (no>0){
+            v.insert(v.begin(),no%2);
+            no = no/2;
+        }
+        int size = v.size();
+        for (int i = size; i<32; i++){
+            v.insert(v.begin(),0);
+        }
+        size = v.size();
+        // for (int i : v){
+        //     cout<<i<<" ";
+        // }
+        for (int i=0; i<size; i++){
+            if(v[i]==1){
+                sum = sum + x;
+            }
+            x=x*2;
+        }
+        return sum;
+    }
+};
